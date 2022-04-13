4caf8e158ca13c5232e8aa496fdcf84e2ed082c3
Sairo Sabater <heroshinsoul@gmail.com>
2022-04-13 18:54:09 +0200

    third test in green

--- src/index.test.ts
+++ src/index.test.ts
@@ -14,10 +14,19 @@ describe("Shiritori Game", () => {
 
     expect(recivedDictionary).toEqual(expectedDictionary);
   })
+
   it("checks if the word is not in the playable dictionary", () => {
 
     const isInTheDictionary = new shiritoriGame().checkWord("book");
 
     expect(isInTheDictionary).toEqual(false);
+  })
+
+  it("checks if, given two words, the last letter from the first word matches the last letter of the second word", () => {
+    const firstWord = "cat"
+    const secondWord = "tongue"
+    const isConcatenated = new shiritoriGame().isConcatenated(firstWord, secondWord);
+
+    expect(isConcatenated).toEqual(true);
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ const playableDictionary = [
   ]
 
 export default class shiritoriGame {
+  isConcatenated(firstWord:string, secondWord:string){
+    return firstWord[firstWord.length-1] === secondWord[0]
+  }
   checkWord(wordToCheck:string){
     return playableDictionary.includes(wordToCheck)
   }
